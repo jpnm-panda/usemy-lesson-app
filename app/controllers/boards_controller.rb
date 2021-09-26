b1c392a6c87bb8df3f65fb640d484cc786b25995
jpnm-panda <jpnm0415shkm@gmail.com>
2021-09-26 17:30:37 +0900

    new-methodを使う準備をしました

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,9 @@
 class BoardsController < ApplicationController
     def index
     end
+
+    # new.html.erb で入力を行うために、Board Class からインスタンスを作成し、@board に代入にしている
+    def new
+        @board = Board.new
+    end
 end
