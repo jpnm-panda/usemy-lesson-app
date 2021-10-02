15315ecf4e2ecd37a501a131a8c1229c259a078a
jpnm-panda <jpnm0415shkm@gmail.com>
2021-10-03 06:56:32 +0900

    やってくれましたね、ルーティングがマッチしないと怒られましたよ

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'boards#index'
   # resources の後に複数形でリソース名をシンボルで指定することで、RESTfull なルーティングを作成する
-  resources :boards, only: [:index, :new, :create, :show, :edit]
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update]
 end
