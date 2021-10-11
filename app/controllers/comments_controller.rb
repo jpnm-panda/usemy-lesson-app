17b6e215e7bc2a2dc65d7b92c8174e4ab69ae909
jpnm-panda <jpnm0415shkm@gmail.com>
2021-10-12 07:24:25 +0900

    コメントを表示させるための処理を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,25 @@
 class CommentsController < ApplicationController
   def create
+      comment = Comment.new(comment_params) # board_id からboard オブジェクトと関連付けられたcomment オブジェクトを作成する
+      if comment.save
+        flash[:notice] = 'コメントを投稿しました'
+
+        redirect_to comment.board # comment オブジェクトに対応した、board_id を取得し紐付いているboard オブジェクトへ遷移させる
+      else
+        redirect_to :back, flash: {
+          comment: comment,
+          error_messages: comment.errors.full_messages
+        }
+      end
   end
 
   def destroy
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:board_id, :name, :comment)
+  end
+
 end
