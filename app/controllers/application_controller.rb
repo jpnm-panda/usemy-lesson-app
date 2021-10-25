410bc36767a579c1492a80fb7e0adeac33b6f5a4
jpnm-panda <jpnm0415shkm@gmail.com>
2021-10-25 21:42:25 +0900

    ログインしているUserにはマイページを表示させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :current_user
+
+    private
+
+    def current_user
+        return unless session[:user_id] # seesion にuser_id の値がない(ログインしていない)場合はreturn している
+        @current_user = User.find_by(id: session[:user_id]) # find_by でuser_id を取得できた(ログインしていた)場合は@current_user に値が格納される
+    end
 end
